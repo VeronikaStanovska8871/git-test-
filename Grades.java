a38910d8a270a52e5aed1ec6069489b1c2ebce56
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-20 10:48:52 +0100

    Update Grades.java
    changed

--- Grades.java
+++ Grades.java
@@ -4,10 +4,12 @@ public class Grades {
     private int math;
     private int eng;
     private int pro;
+    private int bio;
 
     public Grades(int math, int eng, int pro) {
         this.math = math;
         this.eng = eng;
+        //to do add bio
         this.pro = pro;
     }
 
@@ -22,6 +24,8 @@ public class Grades {
     public int getPro() {
         return pro;
     }
+    
+    // override
     @Override
     public String toString(){
         return "Grades: MATH: "+math+" ENG: "+eng+" PRO: "+pro;
@@ -30,5 +34,10 @@ public class Grades {
     public double average(){
         return (double) (math+eng+pro)/3;
     }
+    
+    public int getValue(){
+        retuirn 5;
+    }
+    //koniec
 
 }
